ac7e013aaf57b8f70eabcba39ea17e378f88011f
ddsha441981 <ddsa44191@gmail.com>
2025-04-28 00:33:42 +0530

    Project Setup with template design pattern

---
+++ template-desgin-pattern/src/main/java/com/cwc/template/pattern/TemplateDesginPatternApplication.java
@@ -0,0 +1,13 @@
+package com.cwc.template.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateDesginPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateDesginPatternApplication.class, args);
+	}
+
+}
